0306f7990d218a4cea68f76ba9d61a173f047566
Divyanshu Shekhar <114979302+Divyanshu-Shekha@users.noreply.github.com>
2024-08-27 19:28:43 +0530

    new data

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,34 +1,62 @@
-import React,{useState} from 'react';
-import {Form, useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import { Form, useNavigate } from "react-router-dom";
 
-import {preview} from "../assets";
-import {getRandomPrompt} from '../utils';
-import { FormField , Loader} from "../components";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
   const navigate = useNavigate();
-  const [form,setForm] = useState({
-    name:"",
-    prompt:"",
-    photo:"",
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {};
+  const handleChange = (e) => {};
+  const handleSurpriseMe = () =>{};
   return (
-    <section className='max-w-7xl mx-auto'>
-    <div>
-    <div>
+    <section className="max-w-7xl mx-auto">
+      <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">
           The Community Showcase
         </h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
-          Create imagination and visuallystunning images
-          through IMAGE-GEN-AI and share them with the community
+          Create imagination and visuallystunning images through IMAGE-GEN-AI
+          and share them with the community
         </p>
       </div>
-    </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            LableName="You'r Name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            LableName="Prompt"
+            type="text"
+            name="Prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+          <div className="relative bg-gray-50 border-gray-300 text-gray-900  text-sm rounded-md focus:ring-blue-500
+          focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+              {form.photo ? (<img src={form.photo} alt={form.prompt} className="w-full h-full object-contain"/>) 
+              :(<img src={preview} alt="preview" className="w-9/12 h-9/12 object-contain opacity-40"/>)}
+          </div>
+        </div>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
